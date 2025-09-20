5103b4cd855a749d85c232ecf14b736a7fc3a316
Vinicius Oliveira <fukhaos@gmail.com>
2025-09-20 09:14:56 -0300

    Add FlashList with styled items to main screen
    Introduced a FlashList component in Index screen to display a list of items with chevron icons. Added new styles for the list and its items in styles/index.ts to improve layout and appearance.

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,29 @@
 import { styles } from "@/styles";
-import { TextInput, View } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { FlashList } from "@shopify/flash-list";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
+  const data = [{
+    title: "Um"
+  }, {
+    title: "Dois"
+  }, {
+    title: "Tres"
+  }, {
+    title: "Quatro"
+  }]
+
+  const renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity style={styles.item}>
+        <Text style={styles.item_text}>{item.title}</Text>
+        <Ionicons name="chevron-forward" size={18} color="gray" />
+      </TouchableOpacity>
+    )
+  }
+
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -9,6 +31,13 @@ export default function Index() {
         placeholder="Buscar ..."
         placeholderTextColor="#666"
       />
+
+      <FlashList
+        style={styles.list}
+        data={data}
+        renderItem={renderItem}
+      />
+
 
     </View>
   );
--- styles/index.ts
+++ styles/index.ts
@@ -15,5 +15,25 @@ export const styles = StyleSheet.create({
         borderColor: '#333',
         borderWidth: 1,
         borderRadius: 8,
+    },
+    list: {
+        width: '100%',
+        flex: 1,
+        marginTop: 16
+    },
+    item: {
+        justifyContent: 'space-between',
+        height: 60,
+        backgroundColor: "#F4f4f4",
+        alignItems: 'center',
+        flexDirection: 'row',
+        paddingLeft: 16,
+        paddingRight: 8,
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: '#999'
+    },
+    item_text: {
+        fontWeight: 'bold'
     }
+
 })
